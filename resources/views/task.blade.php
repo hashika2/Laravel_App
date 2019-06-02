45139cf198065e4d0b52f8a19191bc40f9e4e66f
hashika maduranga <37840298+hashika2@users.noreply.github.com>
2019-06-02 12:19:36 -0700

    laravel_App

---
+++ resources/views/task.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+<div class="container">
+<div class="text-center">
+<h1>Task center</h1>
+<div class="row">
+    <div class="col-md-12">
+        <input type="text" class="form-control" name="task" placeholder="Enter your Task">
+        <br>
+            <input type="button" class="btn btn-primary " value="Save">
+            <input type="button" class="btn btn-warning " value="Clear">
+            <table class="table table-dark">
+            <th>ID</th>
+            <th>Completed</th>
+            <th>Task</th>
+            <tr>
+            <td>1</td>
+            <td>I have to learn larevel Today</td>
+            <td>Not yet</td>
+            </tr>
+            </table>
+    </div>
+</div>
+</div>
+</div>
+    
+</body>
+</html>
